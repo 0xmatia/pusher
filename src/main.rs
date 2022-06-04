f12d081c2aaf3b2041b70a60e721cc00c5c38834
Elad Matia <elad.matia@gmail.com>
2022-06-05 00:50:56 +0300

    Initial release. Very much still WIP

--- src/main.rs
+++ src/main.rs
@@ -14,27 +14,138 @@
 
 
 mod tty;
-mod errors;
 
+use std::fs;
+use std::io::Write;
+use std::os::unix::prelude::RawFd;
+use std::thread::sleep;
+use std::time::Duration;
 use std::{env, io};
 use std::path::{PathBuf, Path};
-use anyhow::{Result, anyhow};
+use anyhow::{Result, anyhow, bail};
 use libc::{isatty, open, 
     O_RDWR, // read-write
     O_NONBLOCK, // non blocking
     O_NOCTTY // open as non-controlling terminal
 };
 
-use tty::TTYPort;
-use errors::PusherErrors::{
-    IOError,
-};
+use mio::{Poll, Events, Token, Interest};
+use tty::{SerialDevice, StdinDevice};
+
+const PUSHER_LOGO: &str = r#"
+__________             .__                  
+\______   \__ __  _____|  |__   ___________ 
+ |     ___/  |  \/  ___/  |  \_/ __ \_  __ \
+ |    |   |  |  /\___ \|   Y  \  ___/|  | \/
+ |____|   |____//____  >___|  /\___  >__|   
+                     \/     \/     \/       
+"#;
+const SERIAL_TOKEN: Token = Token(0);
+const STDIN_TOKEN: Token = Token(1);
 
 fn main() -> Result<()> {
-    let (device, kernel_path) = parse_input()?;
+    println!("{}\n[PUSHER] Pusher is waiting...", PUSHER_LOGO);
+    let (serial_fd, baud_rate, kernel_path) = parse_input()?;
+    let mut serial_device = match SerialDevice::init(serial_fd, baud_rate) {
+        Ok(device) => device,
+        Err(err) => bail!("Error opening serial device: {}", err)
+    };
+    let mut stdin_device = match StdinDevice::init() {
+        Ok(stdin) => stdin,
+        Err(err) => bail!("Failed initializing stdin: {}", err)
+    };
+    run(&mut serial_device, &mut stdin_device, kernel_path)?;
     Ok(())
 }
 
+
+fn run(serial_device: &mut SerialDevice, stdin_device: &mut StdinDevice, kernel_path: PathBuf) -> Result<()> {
+    let mut poll = Poll::new()?;
+    let mut events = Events::with_capacity(1024);
+
+    // register serial port and stdin for polling
+    poll.registry().register(serial_device, SERIAL_TOKEN, Interest::READABLE)?;
+    poll.registry().register(stdin_device, STDIN_TOKEN, Interest::READABLE)?;
+
+    let mut num_breaks = 0;
+    loop {
+        poll.poll(&mut events, None)?;
+        for event in &events {
+            match event.token() {
+                SERIAL_TOKEN => {
+                    let output_bytes = serial_device.read_all()?;
+                    print!("{}", String::from_utf8_lossy(&output_bytes));
+
+                    num_breaks += output_bytes.iter().filter(|&byte| *byte == 3).count();
+                    if num_breaks == 3 {
+                        io::stdout().flush()?; 
+                        println!("[PUSHER] Sending kernel!"); 
+                        num_breaks = 0;
+                        send_kernel(serial_device, &kernel_path)?;
+                        let output_bytes = serial_device.read_all()?;
+                        print!("{}", String::from_utf8_lossy(&output_bytes));
+                    }
+                },
+                STDIN_TOKEN => {
+                    // read from stdin and write to serial 
+                    let byte = stdin_device.read()?;
+                    let bytes_written = serial_device.write_byte(byte as u8)?;
+                    if bytes_written != 1 {
+                        dbg!("weird");
+                    }
+                },
+                Token(_) => eprintln!("Unknown token.")
+            }
+        }
+    }
+}
+
+fn send_kernel(serial_device: &mut SerialDevice, kernel_path: &PathBuf) -> Result<()> {
+    // first, send the size of the kernel as the device expects it 
+    let kernel_size = fs::metadata(kernel_path)?.len() as u32;
+    println!("[PUSHER] Kernel size: {}", kernel_size);
+    assert!(std::u32::MAX > kernel_size);
+
+    for i in 0..=3 {
+        let s = ((kernel_size >> 8 * i) & 0xff) as u8;
+        serial_device.write_byte(s)?;
+    }
+    
+    sleep(Duration::from_secs(1)); // Nasty hack, but sometimes read just returns nothing...
+    serial_device.flush()?;
+
+    // now read the response
+    let mut res = serial_device.read_all()?;
+
+    // if "OK" didn't arrive, poll for read events for 5 seconds
+    if res != vec!['O' as u8, 'K' as u8] {
+        dbg!("Didn't receive OK, so polling for 5 seconds");
+        let mut event = Events::with_capacity(1);
+        let mut poll = Poll::new()?;
+        poll.registry().register(serial_device, SERIAL_TOKEN, Interest::READABLE)?;
+        poll.poll(&mut event, Some(Duration::from_secs(5)))?;
+        if event.is_empty() {
+            bail!("Didn't receive OK after sending the kernel size, aborting now")
+        }
+        res.append(&mut serial_device.read_all()?);
+    }
+
+    dbg!(&res);
+    if res != vec!['O' as u8, 'K' as u8] {
+        bail!("Didn't receive OK after sending the kernel size, aborting now")
+    }
+    println!("[PUSHER] Got response: \"{}\", sending image now!", String::from_utf8_lossy(&res));
+
+    // send image now!
+    let kernel_image = fs::read(kernel_path)?;
+
+    for byte in 0..kernel_size {
+        serial_device.write_byte(kernel_image[byte as usize])?;
+    }
+
+    println!("[PUSHER] Done! booting now\n\n");
+    Ok(())
+}
 
 /// Parse command line arguments.
 /// Checks if device exists and is a tty and if the kernel image exists
@@ -44,31 +155,30 @@ fn main() -> Result<()> {
 ///
 /// # Return
 /// The tty device as a `TTYPort` and a path to the kernel image
-fn parse_input() -> Result<(TTYPort, PathBuf)> {
+fn parse_input() -> Result<(RawFd, u32, PathBuf)> {
     let supplied_arguments: Vec<String>  = env::args().collect();
-    if supplied_arguments.len() != 3 {
-        return Err(anyhow!("Usage: pusher <device> <kernel>"));
+    if supplied_arguments.len() != 4 {
+        return Err(anyhow!("Usage: pusher <device> <baudrate> <kernel>"));
     }
     // check if the supplied device exists
     if !Path::new(&supplied_arguments[1]).exists() {
-        return Err(IOError("Device doesn't exists".into()).into());
+        return Err(anyhow!("Device doesn't exists"));
     }
     // check if device is a tty
     let fd;
     unsafe {
         fd = open(supplied_arguments[1].as_ptr() as *const i8, O_RDWR | O_NOCTTY | O_NONBLOCK);
         if -1 == fd {
-            return Err(IOError(format!("Couldn't open device: {}", io::Error::last_os_error())
-                    .into()).into());
+            return Err(anyhow!(format!("Couldn't open device: {}", io::Error::last_os_error())));
         }
         if 0 == isatty(fd) {
-            return Err(IOError("Supplied device is not a tty!".into()).into());
+            return Err(anyhow!("Supplied device is not a tty!"));
         }
     }
     // check the the binary to push exists
-    if !Path::new(&supplied_arguments[2]).exists() {
-        return Err(IOError(format!("{} doesn't exist", supplied_arguments[2]).into()).into());
+    if !Path::new(&supplied_arguments[3]).exists() {
+        return Err(anyhow!(format!("{} doesn't exist", supplied_arguments[2])));
     }
-    Ok((TTYPort::new(fd), PathBuf::from(&supplied_arguments[2])))
+    Ok((fd, supplied_arguments[2].parse::<u32>()?, PathBuf::from(&supplied_arguments[3])))
 }               
 
--- src/tty.rs
+++ src/tty.rs
@@ -1,25 +1,122 @@
-use std::io::Error;
-use libc::close;
+use std::fs::File;
+use std::io::{self, Read, stdin, Write};
+use std::os::unix::prelude::{RawFd, FromRawFd, AsRawFd};
+use std::thread::sleep;
+use std::time::Duration;
+use anyhow::Result;
+use mio::unix::SourceFd;
+use termios::*;
+use mio::{event, Registry, Token, Interest};
 
-/// Represents a serial / UART port 
-pub struct TTYPort {
-    fd: i32 
+/// Represents a serial / UART port
+pub struct SerialDevice {
+    device: File,
+    _baudrate: u32
 }
 
-impl TTYPort {
-    /// Create a new TTYPort, using the supplied fd.
-    pub fn new(fd: i32) -> Self {
-        TTYPort { fd }
+pub struct StdinDevice(RawFd);
+
+impl SerialDevice {
+    pub fn init(serial_fd: RawFd, baudrate: u32) -> io::Result<Self> {
+        let mut termios = Termios::from_fd(serial_fd)?;
+        termios.c_cc[VTIME] = 0;
+        termios.c_cc[VMIN] = 0;
+
+        termios.c_iflag = 0;
+        termios.c_oflag = 0;
+        termios.c_cflag = CS8 | CREAD | CLOCAL; // 8n1
+        termios.c_lflag = !ICANON;
+
+        // set baudrate
+        cfsetspeed(&mut termios, baudrate)?;
+        tcsetattr(serial_fd, TCSANOW, &termios)?;
+        Ok(Self {
+            device: unsafe { File::from_raw_fd(serial_fd) },
+           _baudrate: baudrate
+        } )
+    }
+
+    /// Read until EOF from device, return vector of bytes read.
+    pub fn read_all(&mut self) -> Result<Vec<u8>, io::Error> {
+        let mut buffer = Vec::new();
+        self.device.read_to_end(&mut buffer)?;
+        Ok(buffer)
+    }
+
+    /// Flush
+    pub fn flush(&mut self) -> Result<(), io::Error> {
+        self.device.flush()?;
+        Ok(())
+    }
+
+    /// Write one byte to serial device, and flush
+    pub fn write_byte(&mut self, byte: u8) -> io::Result<usize> {
+        let bytes_written = self.device.write(&[byte])?;
+        sleep(Duration::from_millis(2));
+        Ok(bytes_written)
     }
 }
 
-impl Drop for TTYPort {
-    /// Implement drop the  close the fd automatically when the object goes out of scope.
-    fn drop(&mut self) {
-        unsafe {
-            if 0 != close(self.fd) {
-                eprintln!("Failed closing tty: {}", Error::last_os_error());
-            }
-        }
+/// Implement event source for SerialDevice to be able to register it 
+/// in the Registry and Poll
+impl event::Source for SerialDevice {
+   fn register(&mut self, registry: &Registry, token: Token, interests: Interest)
+        -> io::Result<()>
+    {
+        SourceFd(&self.device.as_raw_fd()).register(registry, token, interests)
+    }
+
+    fn reregister(&mut self, registry: &Registry, token: Token, interests: Interest)
+        -> io::Result<()>
+    {
+        SourceFd(&self.device.as_raw_fd()).reregister(registry, token, interests)
+    }
+
+    fn deregister(&mut self, registry: &Registry) -> io::Result<()> {
+        SourceFd(&self.device.as_raw_fd()).deregister(registry)
     }
 }
+
+impl StdinDevice { 
+    /// Setup stdin for serial communication:
+    /// - Turn terminal echo off. Unless the "otherside" returns the output, nothing will be shown.
+    /// - Turn off canonical mode. This means read doesn't wait for NL to proceed.
+    pub fn init() -> io::Result<Self> {
+        let mut termios = Termios::from_fd(stdin().as_raw_fd())?;
+
+        // disable canonical mode and turn echo off
+        termios.c_lflag &= !(ECHO | ICANON);
+
+        tcsetattr(stdin().as_raw_fd(), TCSANOW, &termios)?;
+        Ok(Self(stdin().as_raw_fd()))
+    }
+
+    /// Read from stdin one byte.
+    pub fn read(&mut self) -> Result<char, io::Error> {
+        let mut buffer = Vec::new();
+        buffer.resize(1, 0);
+        stdin().lock().read_exact(&mut buffer)?;
+        Ok(buffer[0] as char)
+    }
+}
+
+/// Implement event source for StdinDevice to be able to register it 
+/// in the Registry and Poll
+impl event::Source for StdinDevice {
+   fn register(&mut self, registry: &Registry, token: Token, interests: Interest)
+        -> io::Result<()>
+    {
+        SourceFd(&self.0).register(registry, token, interests)
+    }
+
+    fn reregister(&mut self, registry: &Registry, token: Token, interests: Interest)
+        -> io::Result<()>
+    {
+        SourceFd(&self.0).reregister(registry, token, interests)
+    }
+
+    fn deregister(&mut self, registry: &Registry) -> io::Result<()> {
+        SourceFd(&self.0).deregister(registry)
+    }
+}
+
